5d4d928213e1a421e83aec29c75e76b443482662
bondar.a <bondar.a@roistat.com>
2023-11-10 09:39:01 +0300

    Доработал генерацию nginx конфига и папок с файлами узлов

--- app/Helpers/DockerComposeBuilder.php
+++ app/Helpers/DockerComposeBuilder.php
@@ -4,6 +4,8 @@ namespace App\Helpers;
 
 use App\Models\Node;
 use FilesystemIterator;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Facades\Storage;
 use JetBrains\PhpStorm\NoReturn;
 use RecursiveDirectoryIterator;
 use function PHPUnit\Framework\directoryExists;
@@ -13,20 +15,22 @@ class DockerComposeBuilder
     protected array $config;
     protected array $parsedNodeBuildSample;
     protected array $build;
-    protected array $paths;
+    protected array $paths = ['base' => '/var/www/html/project'];
 
     public function __construct(array $config, array $build, array $parsedNodeBuildSample)
     {
         $this->config = $config;
         $this->build = $build;
         $this->parsedNodeBuildSample = $parsedNodeBuildSample;
-//        TODO
-//        $this->paths = [
-//            'nginx_config' => '/var/www/html/project/docker/nginx/nginx.conf',
-//            'nginx_config_full_sample' => '/var/www/html/project/docker/nginx/nginx.sample.conf',
-//            'nginx_config_node_sample' => '/var/www/html/project/nodes/node_sample/docker/nginx/server.sample.conf',
-//            'root_path' => "/var/www/html/project/nodes/node_{$nodeNumber}/public",
-//        ];
+        $this->paths = array_merge($this->paths, [
+            'build' => "{$this->paths['base']}/build.yml",
+            'nodes' => "{$this->paths['base']}/nodes",
+            'nginx_config' => "{$this->paths['base']}/docker/nginx/nginx.conf",
+            'nginx_config_sample_full' => "{$this->paths['base']}/docker/nginx/nginx.sample.conf",
+            'nginx_config_sample_node' => "{$this->paths['base']}/nodes/node_sample/docker/nginx/server.sample.conf",
+            'node_root' => '/public',
+            'node_prefix' => '/node_',
+        ]);
     }
 
     #[NoReturn] public function build(array $defaultServices = []): void
@@ -34,13 +38,16 @@ class DockerComposeBuilder
         $nodes = Node::all();
         $serversCount = round(count($nodes) / $this->config['nodes_per_server']);
 
+        $this->refreshNginxConf();
+        $this->refreshNodesFolder();
+
         for ($i = 1; $i <= $serversCount; $i++) {
             foreach ($this->parsedNodeBuildSample['services'] as $serviceName => $service) {
                 if (!$this->isNeedSerialNumberIn($serviceName)) {
                     continue;
                 }
 
-                $this->build['services']["{$serviceName}{$i}"] = $this->serviceKeyIdentifierRecursive($service, $i);
+                $this->build['services']["{$serviceName}{$i}"] = $this->identifyServiceKeyRecursive($service, $i);
             }
         }
         foreach ($this->build['services'] as $serviceKey => $service) {
@@ -73,10 +80,30 @@ class DockerComposeBuilder
             }
         }
         $this->compactNginxPorts();
-        dd($this->build);
-        yaml_emit_file('/var/www/html/project/build.yml', $this->build);
 
-        echo file_get_contents('/var/www/html/project/build.yml');
+        dd($this->build);
+
+        yaml_emit_file($this->paths['build'], $this->build);
+
+        echo file_get_contents($this->paths['build']);
+    }
+
+    protected function refreshNginxConf(): void
+    {
+        file_put_contents($this->paths['nginx_config'], file_get_contents($this->paths['nginx_config_sample_full']));
+    }
+
+    protected function refreshNodesFolder(): void
+    {
+        $fs = new Filesystem;
+
+        foreach ($fs->directories($this->paths['nodes']) as $path) {
+            if (str_contains($path, 'node_sample')) {
+                continue;
+            }
+
+            $fs->deleteDirectory($path);
+        }
     }
 
     protected function putNodeToNginxConf(string $serviceKey, int $nodeNumber): void
@@ -84,18 +111,16 @@ class DockerComposeBuilder
         $expose = max(array_map(function ($port) {
             return (int)$port;
         }, $this->build['services']['nginx']['ports']));
-        $expose++;
-        $configPathServerSample = '/var/www/html/project/docker/nginx/nginx.sample.conf';
-        $configPathServer = '/var/www/html/project/docker/nginx/nginx.conf';
-        $configPathNode = '/var/www/html/project/nodes/node_sample/docker/nginx/server.sample.conf';
-        $configSampleOfNode = file_get_contents($configPathNode);
-        $configOfServer = file_get_contents($configPathServer);
+        $configOfNode = file_get_contents($this->paths['nginx_config_sample_node']);
+        $configOfServer = file_get_contents($this->paths['nginx_config']);
         $toReplace = [
             'location_name' => $serviceKey,
-            'root_path' => "/var/www/html/project/nodes/node_{$nodeNumber}/public",
-            'listen_port' => $expose,
+            'listen_port' => ++$expose,
+            'root_path' => $this->paths['nodes']
+                . $this->paths['node_prefix']
+                . $nodeNumber
+                . $this->paths['node_root'],
         ];
-        $configOfNode = $configSampleOfNode;
 
         foreach ($toReplace as $directive => $value) {
             $configOfNode = str_replace("{{ {$directive} }}", $value, $configOfNode);
@@ -103,25 +128,25 @@ class DockerComposeBuilder
 
         $this->build['services']['nginx']['ports'][] = $expose . ':' . $expose;
         file_put_contents(
-            $configPathServer,
+            $this->paths['nginx_config'],
             substr($configOfServer,0,-2) . $configOfNode . '}' . PHP_EOL);
     }
 
     protected function compactNginxPorts(): void
     {
-        $fPort = (int)$this->build['services']['nginx']['ports'][0];
-        $lPort = (int)$this->build['services']['nginx']['ports'][0]
+        $portFirst = (int)$this->build['services']['nginx']['ports'][0];
+        $portLast = (int)$this->build['services']['nginx']['ports'][0]
             + count($this->build['services']['nginx']['ports'])
             - 1;
         $this->build['services']['nginx']['ports'] = [
-            "{$fPort}-{$lPort}:{$fPort}-{$lPort}"
+            "{$portFirst}-{$portLast}:{$portFirst}-{$portLast}"
         ];
     }
 
     protected function buildNodeDir(int $nodeNumber): void
     {
-        $nodeSampleDir = '/var/www/html/project/nodes/node_sample';
-        $nodeDir = "/var/www/html/project/nodes/node_{$nodeNumber}";
+        $nodeSampleDir = "{$this->paths['nodes']}/node_sample";
+        $nodeDir = $this->paths['nodes'] . $this->paths['node_prefix'] . $nodeNumber;
 
         if (!file_exists($nodeDir)) {
             mkdir($nodeDir, 0755);
@@ -143,7 +168,7 @@ class DockerComposeBuilder
         }
     }
 
-    protected function serviceKeyIdentifierRecursive(array $service, int $id): array
+    protected function identifyServiceKeyRecursive(array $service, int $id): array
     {
         $identifiedService = [];
 
@@ -151,14 +176,14 @@ class DockerComposeBuilder
             $key = $this->isNeedSerialNumberIn($attribute) ? "{$attribute}{$id}" : $attribute;
             $identifiedService[$key] =
                 is_array($value)
-                    ? $this->serviceKeyIdentifierRecursive($value, $id)
-                    : $this->serviceValueIdentifier($value, $id);
+                    ? $this->identifyServiceKeyRecursive($value, $id)
+                    : $this->identifyServiceValue($value, $id);
         }
 
         return $identifiedService;
     }
 
-    protected function serviceValueIdentifier(string $value, int $id): string
+    protected function identifyServiceValue(string $value, int $id): string
     {
         if ($this->isNeedSerialNumberIn($value)) {
             preg_match('/node_/', $value, $matches);
